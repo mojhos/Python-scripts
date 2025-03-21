c818f12dd906e3980cba5977c4da774a488bd5b9
mojhos <s.mojtaba.h65@gmail.com>
2025-03-21 17:31:44 -0400

    update with filter on volumes tags

--- volume_backup.py
+++ volume_backup.py
@@ -4,8 +4,15 @@ import schedule
 ec2_client = boto3.client('ec2', region_name='us-east-1')
 
 def create_snapshot_from_volumes():
-    volumes = ec2_client.describe_volumes()['Volumes']
-    for volume in volumes:
+    volumes = ec2_client.describe_volumes(
+        Filters=[
+            {
+                'Name': 'tag:Name',
+                'Values': ['prod']
+            }
+        ]
+    )
+    for volume in volumes['Volumes']:
         ec2_client.create_snapshot(
             VolumeId=volume['VolumeId']
         )
