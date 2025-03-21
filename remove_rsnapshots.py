0b715258fc711617cb277e60d10e08b26efe4b03
mojhos <s.mojtaba.h65@gmail.com>
2025-03-21 18:38:05 -0400

    update with filter on volumes tags

--- remove_rsnapshots.py
+++ remove_rsnapshots.py
@@ -5,11 +5,30 @@ import operator
 
 ec2_client = boto3.client('ec2', region_name='us-east-1')
 
-snapshots = ec2_client.describe_snapshots(
-    OwnerId=['self']
+# we make a list of volumes based on special volume tag.
+volumes = ec2_client.describe_volumes(
+    Filters=[
+        {
+            'Name': 'tag:Name',
+            'Values': ['prod']
+        }
+    ]
 )
-sorted_snapshots = sorted(snapshots['Snapshots'], key=itemgetter('StartTime'), reverse=True)
-for snap in sorted_snapshots[2:]:               ##we keep the two recent snapshots and remove the rest
-    ec2_client.delete_snapshot(
-        SnaphsotId= snap['SnapshotId']
+#make a list of all snapshots for each volume we found in previous list and find them by VolumeId filtering
+for volume in volumes['Volumes']:
+    snapshots = ec2_client.describe_snapshots(
+        OwnerId=['self'],
+        Filters=[
+            {
+                'Name': 'volume-id',
+                'Values': [volume['VolumeId']]
+            }
+        ]
     )
+    sorted_snapshots = sorted(snapshots['Snapshots'], key=itemgetter('StartTime'), reverse=True)
+
+    ##we keep the two recent snapshots and remove the rest of them
+    for snap in sorted_snapshots[2:]:
+        ec2_client.delete_snapshot(
+            SnaphsotId=snap['SnapshotId']
+        )
